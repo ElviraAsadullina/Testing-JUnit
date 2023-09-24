f3b90c3a876408caba248380ed613f3a66b5b55c
Elvira <eliasufa@yahoo.com>
2023-09-24 14:32:46 +0500

    Homework 2 completed

---
+++ src/test/VehicleTest.java
@@ -0,0 +1,59 @@
+import main.java.Homework2.Task1.Car;
+import main.java.Homework2.Task1.Motorcycle;
+import main.java.Homework2.Task1.Vehicle;
+
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class VehicleTest {
+    Car car;
+    Motorcycle motorcycle;
+
+    @BeforeEach
+    void prepareData() {
+        car = new Car("Toyota", "Lexus LC 500", 2018);
+        motorcycle = new Motorcycle("Suzuki", "V-Strom 650 ABS", 2017);
+    }
+
+    @Test
+    void carIsInstanceOfVehicleTest() {
+        assertThat(car).isInstanceOf(Vehicle.class);
+    }
+
+    @Test
+    void carObjectHasFourWheelsTest() {
+        assertThat(car.getNumWheels()).isEqualTo(4);
+    }
+
+    @Test
+    void motorcycleObjectHasTwoWheelsTest() {
+        assertThat(motorcycle.getNumWheels()).isEqualTo(2);
+    }
+
+    @Test
+    void carCheckTestDriveSpeedTest() {
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+
+    @Test
+    void motorcycleCheckTestDriveSpeedTest() {
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+    }
+
+    @Test
+    void carCheckParkModeSpeedTest() {
+        car.testDrive();
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+
+    @Test
+    void motorcycleCheckParkModeSpeedTest() {
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertThat(motorcycle.getSpeed()).isEqualTo(0);
+    }
+}
